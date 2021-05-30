9d8750b9b59b663b404e7b0fa6f4fa0f2f10771e
Zvampish <nornostvold02@gmail.com>
2021-05-30 12:21:48 +0200

    Ändrade en kommentar

--- Slutprojektet2/Char.cs
+++ Slutprojektet2/Char.cs
@@ -7,8 +7,10 @@ namespace Slutprojektet2
     {
         // Genererar ett random string genom att slumpa en följd av tecken från variabeln chars
         // Längden på den slumpade stringen sätts som parameter för metoden med inten length
+        
         public string RandomString(int length)
         {
+            //Inte skriven av mig. Det under den här kommentaren är tagen från stack-overflow
             string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
             return new string(Enumerable.Repeat(chars, length)
             .Select(s => s[Program.generator.Next(s.Length)]).ToArray());
